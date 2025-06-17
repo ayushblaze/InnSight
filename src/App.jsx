9a44b2bc0bdbd59e8db3dc23302a5e248fe23d6f
ayushblaze <ayushmoca@gmail.com>
2025-06-17 17:28:20 +0530

    feat(row & button): create row and button components and setup conditional styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.main`
-  background-color: red;
+  // background-color: red;
   padding: 20px;
 `;
 
@@ -14,17 +15,25 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">Hello world</Heading>
-        <Heading as="h2">Check in and out</Heading>
-        <Button onClick={() => alert("Check in!")}>Check in</Button>
-        <Button onClick={() => alert("Check out!")}>Check out</Button>
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">Hello world</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("Check in!")}>Check in</Button>
+              <Button variation="secondary" size="small" onClick={() => alert("Check out!")}>Check out</Button>
+            </div>
+          </Row>
 
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guests" />
-        <Input type="number" placeholder="Number of guests" />
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <Input type="number" placeholder="Number of guests" />
+            <Input type="number" placeholder="Number of guests" />
+          </Row>
+        </Row>  
       </StyledApp>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
