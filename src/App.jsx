6979825c45801756323cb6048aaa0ab715191c55
ayushblaze <ayushmoca@gmail.com>
2025-06-17 17:02:04 +0530

    feat(heading): make use of the as prop to make correct html element is rendered

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,12 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading type="h1">Hello world</Heading>
-        <Heading type="h2">Check in and out</Heading>
+        <Heading as="h1">Hello world</Heading>
+        <Heading as="h2">Check in and out</Heading>
         <Button onClick={() => alert("Check in!")}>Check in</Button>
         <Button onClick={() => alert("Check out!")}>Check out</Button>
 
-        <Heading type="h3">Form</Heading>
+        <Heading as="h3">Form</Heading>
         <Input type="number" placeholder="Number of guests" />
         <Input type="number" placeholder="Number of guests" />
       </StyledApp>
