b3606ed0af02985c8e89368c439a73e658ee5a17
ayushblaze <ayushmoca@gmail.com>
2025-06-17 20:28:43 +0530

    feat(icons): add application logo and icons for Sidebar links

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -11,7 +13,8 @@ const StyledSidebar = styled.aside`
 function Sidebar() {
   return (
     <StyledSidebar>
-      Sidebar
+      <Logo />
+      <MainNav />
     </StyledSidebar>
   );
 }
