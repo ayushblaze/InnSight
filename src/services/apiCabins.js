19d9ffdc1fd6899742f0706abcdd3c3685f995f2
ayushblaze <ayushmoca@gmail.com>
2025-06-18 23:37:33 +0530

    feat(supabase): setup database connection

---
+++ src/services/apiCabins.js
@@ -0,0 +1,12 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("Cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("There was an error fetching cabins");
+  }
+
+  return data;
+}
---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://xxjutdbqvhpmnvdbwjzb.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4anV0ZGJxdmhwbW52ZGJ3anpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxNzE4OTYsImV4cCI6MjA2NTc0Nzg5Nn0.jbkkfHoSWAeN0ZDjFmiDOjzMBDict8TPVRaznavfyIU";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
