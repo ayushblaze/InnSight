e6042cbcde27d1715fa2c55de2195d77b052527f
ayushblaze <ayushmoca@gmail.com>
2025-06-25 19:48:25 +0530

    feat(cabins): render props technique

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -6,21 +6,16 @@ import CabinTable from "../cabins/CabinTable";
 
 function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open opens='cabin-form'>
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name='cabin-form'>
-        <CreateCabinForm />
-      </Modal.Window>
-
-      <Modal.Open opens='table'>
-        <Button>Show Table</Button>
-      </Modal.Open>
-      <Modal.Window name='table'>
-        <CabinTable />
-      </Modal.Window>
-    </Modal>
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+    </div>
   );
 }
 
