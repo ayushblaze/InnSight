99c427d0520d56788e836e94ee8cbb9e62f96d35
ayushblaze <ayushmoca@gmail.com>
2025-07-04 23:00:21 +0530

    feat(dark-mode): change input field text color for dark mode

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -2,10 +2,18 @@ import styled from "styled-components";
 
 const Input = styled.input`
   border: 1px solid var(--color-grey-300);
-  bacground-color: var(--color-grey-0);
+  background-color: var(--color-grey-0);
   border-radius: var(--border-radius-sm);
   box-shadow: var(--shadow-sm);
   padding: 0.8rem 1.2rem;
+  color: var(--color-grey-900);
+
+  /* Dark mode support */
+  body.dark-mode & {
+    color: var(--color-grey-700);
+    background-color: var(--color-grey-100);
+    border: 1px solid var(--color-grey-400);
+  }
 `;
 
 export default Input;
